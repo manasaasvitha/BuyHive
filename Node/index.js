d75c1d5c5b0ccea8eec812a0a1873c0f361eba5a
Manasa Asvitha CK M <23cseb18manasaasvithackm@gmail.com>
2025-06-27 12:47:45 +0530

    Fix: Added mongoose import

--- Node/index.js
+++ Node/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const cors = require("cors");
 const connectDB = require("./config/db"); 
 const authRoutes = require("./route/auth");
